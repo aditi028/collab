e74a7992584c99b5ac540f4c4147bc103b20a385
aditi028 <AditiKulkarni2899@gmail.com>
2022-08-21 22:48:06 +0530

    adding port

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const io = require('socket.io')(3001,{
     cors:{
-        origin:'http://localhost:3000',
+        origin:'http://localhost:3000' || 'https://collab-live-client.herokuapp.com',
         methods: ['GET','POST']
     }
 })
